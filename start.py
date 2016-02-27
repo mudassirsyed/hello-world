5f06021f86e27e3900607b1616751c1d248c6594
mudassirsyed <mudassir.safdar@gmail.com>
2016-02-27 18:45:03 -0500

    sm

--- start.py
+++ start.py
@@ -9,4 +9,5 @@ print y
 print " but where is the value of z?"
 print " I have no idea!. I donot know.."
 print "But why donot you know? If you know the value of x is %d then you should know the value of z " %x
-print ' But I only know the value of x'
+print ' But I only know the value of x is %d' %y
+print " Hey wait.. you first said that value of x is %d, now you just changed it to %d." %(x,y) 
