1fafe6f0b3df2d05be0793ee6ab2505d326cfbc9
mudassir.safdar <mudassir.safdar@gmail.com>
2016-02-29 09:52:11 -0500

    Added bot/human names

--- start.py
+++ start.py
@@ -2,12 +2,13 @@
 
 x=10
 y = 200
-print "here is the value of x "
+print "BOT:here is the value of x "
 print x
-print "here is the value of y" 
+print "BOT:here is the value of y" 
 print y
-print " but where is the value of z?"
-print " I have no idea!. I donot know.."
-print "But why donot you know? If you know the value of x is %d then you should know the value of z " %x
-print ' But I only know the value of x is %d' %y
-print " Hey wait.. you first said that value of x is %d, now you just changed it to %d." %(x,y) 
+print "HUMAN:but where is the value of z?"
+print "BOT:I have no idea!. I donot know.."
+print "HUMAN:But why donot you know? If you know the value of x is %d then you should know the value of z " %x
+print 'BOT:But I only know the value of x is %d' %y
+print "HUMAN:Hey wait.. you first said that value of x is %d, now you just changed it to %d." %(x,y)
+ 
